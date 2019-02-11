4de90dfa44306f3de0609a08dfd357ea9f3bf9b6
George Tintareanu <tintareanu.george@gmail.com>
2019-02-11 18:36:16 +0100

    Ass 3 - first exercise

--- modules/dh2320lab2/colorinterpolation.cpp
+++ modules/dh2320lab2/colorinterpolation.cpp
@@ -34,9 +34,9 @@ namespace
     {
         ColorSpace::Rgb InterpolatedColor;
 
-        InterpolatedColor.r = ColorA.r;
-        InterpolatedColor.g = t * ColorB.g;
-        InterpolatedColor.b = ColorA.b;
+        InterpolatedColor.r = (1 - t) * ColorA.r + t * ColorB.r;
+        InterpolatedColor.g = (1 - t) * ColorA.g + t * ColorB.g;
+        InterpolatedColor.b = (1 - t) * ColorA.b + t * ColorB.b;
 
         return InterpolatedColor;
     }
@@ -61,10 +61,10 @@ namespace
     {
         ColorSpace::Cmyk InterpolatedColor;
 
-        InterpolatedColor.c = (1-t) * ColorA.c;
-        InterpolatedColor.m = (1-t) * ColorA.m;
-        InterpolatedColor.y = t * ColorB.y;
-        InterpolatedColor.k = t * ColorB.k;
+        InterpolatedColor.c = (1-t) * ColorA.c + t * ColorB.c;
+        InterpolatedColor.m = (1-t) * ColorA.m + t * ColorB.m;
+        InterpolatedColor.y = (1-t) * ColorA.y + t * ColorB.y;
+        InterpolatedColor.k = (1-t) * ColorA.k + t * ColorB.k;
 
         return InterpolatedColor;
     }
@@ -88,9 +88,9 @@ namespace
     {
         ColorSpace::Hsv InterpolatedColor;
 
-        InterpolatedColor.h = 5 * t * ColorB.h;
-        InterpolatedColor.s = ColorA.s + ColorB.s;
-        InterpolatedColor.v = ColorA.v + ColorB.v;
+        InterpolatedColor.h = (1 - t) * ColorA.h + t * ColorB.h;
+        InterpolatedColor.s = (1 - t) * ColorA.s + t * ColorB.s;
+        InterpolatedColor.v = (1 - t) * ColorA.v + t * ColorB.v;
 
         return InterpolatedColor;
     }
@@ -116,9 +116,9 @@ namespace
     {
         ColorSpace::Hsv ChangedColor;
 
-        ChangedColor.h = 220.0;
-        ChangedColor.s = 1.0;
-        ChangedColor.v = 0.9;
+        ChangedColor.h = Color.h;
+        ChangedColor.s = (1-tSaturation) * 0 + tSaturation * 1;
+        ChangedColor.v = (1-tValue) * 0 + tValue * 1;
 
         return ChangedColor;
     }
@@ -144,9 +144,9 @@ namespace
     {
         ColorSpace::Hsv ChangedColor;
 
-        ChangedColor.h = 180.0;
-        ChangedColor.s = 1.0;
-        ChangedColor.v = 0.5;
+        ChangedColor.h = (1-tHue) * 0 + tHue * 360;
+        ChangedColor.s = (1-tSaturation) * 0 + tSaturation * 1;
+        ChangedColor.v = Color.v;
 
         return ChangedColor;
     }
